c738f61c69ec1ff353edb8b42570644931762b6e
Elza <dylan.elza@gmail.com>
2023-06-17 12:56:36 -0500

    Added admin privileges and allow only appropriate authors to edit and delete comments

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const Partner = require('../models/partner');
-const partnerRouter = express.Router();
 const authenticate = require('../authenticate');
+const partnerRouter = express.Router();
+
 
 partnerRouter.route('/')
     .get((req, res, next) => {
@@ -13,7 +14,7 @@ partnerRouter.route('/')
             })
             .catch(err => next(err));
     })
-    .post(authenticate.verifyUser, (req, res, next) => {
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.create(req.body)
             .then(partner => {
                 console.log('partner Created ', partner);
@@ -27,7 +28,7 @@ partnerRouter.route('/')
         res.statusCode = 403;
         res.end('PUT operation not supported on /partners');
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.deleteMany()
             .then(response => {
                 res.statusCode = 200;
@@ -51,7 +52,7 @@ partnerRouter.route('/:partnerId')
         res.statusCode = 403;
         res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
     })
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.findByIdAndUpdate(req.params.partnerId, {
             $set: req.body
         }, { new: true })
@@ -62,7 +63,7 @@ partnerRouter.route('/:partnerId')
             })
             .catch(err => next(err));
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         Partner.findByIdAndDelete(req.params.partnerId)
             .then(response => {
                 res.statusCode = 200;
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const promotion = require('../models/promotion');
-const promotionRouter = express.Router();
 const authenticate = require('../authenticate');
+const promotionRouter = express.Router();
+
 
 promotionRouter.route('/')
     .get((req, res, next) => {
@@ -13,7 +14,7 @@ promotionRouter.route('/')
             })
             .catch(err => next(err));
     })
-    .post(authenticate.verifyUser, (req, res, next) => {
+    .post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         promotion.create(req.body)
             .then(promotion => {
                 console.log('promotion Created ', promotion);
@@ -27,7 +28,7 @@ promotionRouter.route('/')
         res.statusCode = 403;
         res.end('PUT operation not supported on /promotions');
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         promotion.deleteMany()
             .then(response => {
                 res.statusCode = 200;
@@ -51,7 +52,7 @@ promotionRouter.route('/:promotionId')
         res.statusCode = 403;
         res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
     })
-    .put(authenticate.verifyUser, (req, res, next) => {
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         promotion.findByIdAndUpdate(req.params.promotionId, {
             $set: req.body
         }, { new: true })
@@ -62,7 +63,7 @@ promotionRouter.route('/:promotionId')
             })
             .catch(err => next(err));
     })
-    .delete(authenticate.verifyUser, (req, res, next) => {
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
         promotion.findByIdAndDelete(req.params.promotionId)
             .then(response => {
                 res.statusCode = 200;
